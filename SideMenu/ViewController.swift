ab4ec1a1373ae15da565c0fb124633f0e63a9dd9
Mohamed osama <mohamed.osama9812@gmail.com>
2020-10-27 19:31:39 -0700

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Mohamed on 10/27/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
